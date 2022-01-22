1ac4da93a2d35dbd8f4cb559c7d456b35d94620f
Santhu Rao <raoraosanthu@gmail.com>
2022-01-22 19:16:31 +0000

    added custom and local tags

--- src/main.tf
+++ src/main.tf
@@ -3,14 +3,18 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  tags = {
+    Name = "Octopus"
+  }
+}
+
 # Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
 
-  tags = {
-    Name = "main-vpc"
-  }
+  tags = "$merge(var.custom_tags, '${local.tags}-VPC')"
 }
 
 # Create a Subnet
@@ -18,7 +22,5 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "main-subnet"
-  }
+  tags = "$merge(var.custom_tags, '${local.tags}-Subnet')"
 }
--- src/variables.tf
+++ src/variables.tf
@@ -2,3 +2,15 @@ variable "region" {
   type = string
   default = "us-east-1"
 }
+
+variable "custom_tags" {
+  type = map(any)
+  description = "Tags for infrastructure resources."
+
+  default = {
+    Department = "IT"
+    CostCentre = "ABC123"
+    ContactPerson = "andy@tinfoilcipher.co.uk"
+    ManagedByTerraform = "True"
+}
+}
