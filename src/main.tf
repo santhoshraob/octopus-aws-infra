3a8dc963b7b8e9bed8c3e11952310e37681d9e29
Santhu Rao <raoraosanthu@gmail.com>
2022-01-22 19:24:07 +0000

    update tags syntax

--- src/main.tf
+++ src/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
 
-  tags = "$merge(var.custom_tags, '${local.tags}-VPC')"
+  tags = merge(var.custom_tags, "${local.tags}-VPC")
 }
 
 # Create a Subnet
@@ -22,5 +22,5 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags = "$merge(var.custom_tags, '${local.tags}-Subnet')"
+  tags = merge(var.custom_tags, "${local.tags}-Subnet")
 }
